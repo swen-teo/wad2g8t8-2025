a5c5dc5fb781a5d51d5d274d2dd93ff6461dcc27
Nicole <118412377+Goh-Sim-Yng-Nicole@users.noreply.github.com>
2025-10-28 08:37:43 +0000

    modifiy server to specific port 3000

--- quest-pass/vite.config.js
+++ quest-pass/vite.config.js
@@ -10,5 +10,8 @@ export default defineConfig({
       // allow imports using `@/...` to reference the `src` directory
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    port: 3000
   }
 })
