03bf899cb9ea729236ef98ee8190f3caa9f94da9
La Hue Van <huevan.la.2024@smu.edu.sg>
2025-10-30 17:17:58 +0000

    event_database update will continue

--- quest-pass/src/firebase.js
+++ quest-pass/src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+// import { getFirestore } from 'firebase/firestore';
+import { initializeFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 // (You provided this)
@@ -8,7 +9,7 @@ const firebaseConfig = {
   apiKey: 'AIzaSyDrcESDuT2jbQjllA8LXQN8vL9RkpF0qbg',
   authDomain: 'questpass-b3680.firebaseapp.com',
   projectId: 'questpass-b3680',
-  storageBucket: 'questpass-b3680.firebasestorage.app',
+  storageBucket: 'questpass-b3680.appspot.com',
   messagingSenderId: '702360140919',
   appId: '1:702360140919:web:48db31942382151a5e5a72',
   measurementId: 'G-7XLXKQVQ33',
@@ -21,7 +22,9 @@ const app = initializeApp(firebaseConfig);
 // These are what your components (Login, App, Profile, etc.)
 // will import and use.
 export const auth = getAuth(app);
-export const db = getFirestore(app);
-
+// export const db = getFirestore(app);
+export const db = initializeFirestore(app, {
+  experimentalAutoDetectLongPolling: true,  
+});
 // You can also export the main 'app' if you need it elsewhere
 export default app;
